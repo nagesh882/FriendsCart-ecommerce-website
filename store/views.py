08570717d271e4619ba7ac948ea047a2653da7d7
Nagesh <yashchavan192001@gmail.com>
2024-01-12 17:47:46 +0530

    some changes in store/views.py I created variable in_cart for to view cart added or not

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
 from django.shortcuts import get_object_or_404
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 
 
 # Create your views here.
@@ -28,10 +30,12 @@ def store(request, category_slug=None):
 def product_details(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__Cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e :
         raise e
     
     context = {
         'single_product' : single_product,
+        'in_cart' : in_cart
     }
     return render(request, "store/product_detail.html", context)
