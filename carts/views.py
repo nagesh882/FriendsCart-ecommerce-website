eac7cbdc36d454a0d1af283ebf092d83b17d3794
Nagesh <yashchavan192001@gmail.com>
2024-01-13 01:18:37 +0530

    I have fixed products warning and empty cart issue

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -56,6 +57,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(Cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -63,7 +66,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = ( 2.5 * total)/100
         grand_total = total + tax
-    except CartItem.DoesNotExist:
+    except ObjectDoesNotExist:
         pass # Just ignore
     context = {
         'total' : total,
--- store/views.py
+++ store/views.py
@@ -15,13 +15,13 @@ def store(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.all().filter(category=categories, is_available=True)
+        products = Product.objects.all().filter(category=categories, is_available=True).order_by('id')
         paginator = Paginator(products, 1)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         products_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 3)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
