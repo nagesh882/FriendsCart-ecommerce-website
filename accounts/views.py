41913bd3b1eb728d1508a9431ec9ecabed0939ea
Nagesh <yashchavan192001@gmail.com>
2024-01-14 02:49:17 +0530

    User login and new register functionality has been created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from accounts.forms import RegistationForm
+from accounts.models import Account
+from django.contrib import messages
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = RegistationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            user.phone = phone
+            user.save()
+            messages.success(request, 'Registartion Successful.')
+            return redirect('register')
+    else:
+        form = RegistationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, "You are now loged in.")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You are logged out.")
+    return redirect('login')
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,3 +143,9 @@ MEDIAFILES_DIRS =[
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
