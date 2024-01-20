208bf9db2365b9f1b5f9ba9d444c6f105242b625
Nagesh <yashchavan192001@gmail.com>
2024-01-20 19:10:51 +0530

    Checkout and place order has been creted

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,9 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+
 def register(request):
     if request.method == "POST":
         form = RegistationForm(request.POST)
@@ -67,6 +70,16 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(Cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, "You are now loged in.")
             # return redirect('home')
